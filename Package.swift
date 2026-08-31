58868152d8f12afa8089e970452c2395efbcfdb0
Qi Zhang <qi@bota.dev>
2026-08-30 21:20:17 -0700

    build(apple): pin reproducible 1.0.0 artifact

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "BotaDeviceSDKC",
             url: "https://github.com/bota-dev/app-sdk/releases/download/v1.0.0/BotaDeviceSDKCore.xcframework.zip",
-            checksum: "c31dd2fd1c038a2692bcb150014c7e91e4cf6df545c4bc2daefd6847f0bcb3d4"
+            checksum: "6950e76cb40ff5be12e186fb73eff9a52ca8b16f9e388c2d4678145700c82fc4"
         ),
         .target(
             name: "BotaAppleSDK",
